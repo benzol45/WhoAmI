72305766d935aa438b364301c5943593ac29f516
Mikhail Kalinin <mi.kalinin@andersenlab.com>
2025-04-05 22:31:53 +0200

    Security method configs

--- src/main/java/identifier/config/SecurityConfig.java
+++ src/main/java/identifier/config/SecurityConfig.java
@@ -9,14 +9,17 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+//@EnableMethodSecurity
 public class SecurityConfig {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
@@ -34,7 +37,10 @@ public class SecurityConfig {
 
                         .requestMatchers("/profiles").hasRole("ADMIN")
                         .anyRequest().denyAll()
+                        //.anyRequest().authenticated()
                 )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
--- src/main/java/identifier/controller/EmployeeController.java
+++ src/main/java/identifier/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,11 +26,13 @@ public class EmployeeController {
     private IdentifyService identifyService;
 
     @GetMapping
+    //@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public List<EmployeeModel> getAllEmployee() {
         return identifyService.getAllEmployee();
     }
 
     @GetMapping("/{employeeId}")
+    //@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public ResponseEntity<EmployeeModel> findEmployee(@PathVariable("employeeId") Integer employeeId) {
         return identifyService.findEmployee(employeeId)
                 .map(employee -> ResponseEntity.ok(employee))
@@ -37,12 +40,14 @@ public class EmployeeController {
     }
 
     @PostMapping
+    //@PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     public Integer createEmployee(@RequestBody @Valid EmployeeModel employeeModel) {
         return identifyService.createEmployee(employeeModel);
     }
 
     @PutMapping("/{employeeId}")
+    //@PreAuthorize("hasRole('ADMIN')")
     public Integer editEmployee(
             @PathVariable("employeeId") Integer employeeId,
             @RequestBody @Valid EmployeeModel employeeModel) {
