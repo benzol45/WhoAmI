4108332370501219d73d6f4aedae4f3d15d7b807
Mikhail Kalinin <mi.kalinin@andersenlab.com>
2025-07-02 20:53:49 +0200

    swagger disabled

--- src/main/java/identifier/IdentifierApp.java
+++ src/main/java/identifier/IdentifierApp.java
@@ -1,17 +1,15 @@
 package identifier;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(
+/*@OpenAPIDefinition(
         info = @Info(
                 title = "Identifier API",
                 version = "1.0",
                 description = "API documentation"
         )
-)
+)*/
 @SpringBootApplication
 public class IdentifierApp {
     public static void main(String[] args) {
